a3191dc6d883de58ff07909ed6f0095a28a11f33
Sogdiana Rizaeva <141143532+Sogdi24@users.noreply.github.com>
2024-06-04 22:42:36 +0300

    Add files via upload

---
+++ smartbasket/php/config.php
@@ -0,0 +1,18 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
+
+		// *** SMTP *** //
+
+		 require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		 const HOST = 'smtp.mail.ru';
+		 const LOGIN = 'di.riz@mail.ru';
+		 const PASS = '98mvE1dM4Lw8AsHSsLnV';
+		 const PORT = '465';
+
+		// *** /SMTP *** //
+   
+    const SENDER = 'di.riz@mail.ru';
+    const CATCHER = 'rizaevasogdiana6@gmail.ru';
+    const SUBJECT = 'Заявка с сайта';
+    const CHARSET = 'UTF-8';
+    
