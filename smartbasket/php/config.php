495145b93a9ba34553a23636b35b14c8bb432acb
Sogdiana Rizaeva <141143532+Sogdi24@users.noreply.github.com>
2024-06-04 23:22:50 +0300

    Update config.php

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -4,15 +4,15 @@
 		// *** SMTP *** //
 
 		 require_once($_SERVER['DOCUMENT_ROOT'] . './smartbasket/php/phpmailer/smtp.php');
-		 const HOST = 'smtp.mail.ru';
-		 const LOGIN = 'di.riz';
-		 const PASS = '98mvE1dM4Lw8AsHSsLnV';
+		 const HOST = 'smtp.yandex.ru';
+		 const LOGIN = 'rizaevasogdiana@yandex.ru';
+		 const PASS = 'qtvubxzcookjlgcd';
 		 const PORT = '465';
 
 		// *** /SMTP *** //
    
-    const SENDER = 'di.riz@mail.ru';
-    const CATCHER = 'di.riz@mail.ru';
+    const SENDER = 'rizaevasogdiana@yandex.ru';
+    const CATCHER = 'rizaevasogdiana@yandex.ru';
     const SUBJECT = 'Заявка с сайта';
     const CHARSET = 'UTF-8';
     
