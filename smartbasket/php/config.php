b2b704551d83a15fd9ceb85870d9446119e36e38
Sogdiana Rizaeva <141143532+Sogdi24@users.noreply.github.com>
2024-06-04 23:16:11 +0300

    Update config.php

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -5,14 +5,14 @@
 
 		 require_once($_SERVER['DOCUMENT_ROOT'] . './smartbasket/php/phpmailer/smtp.php');
 		 const HOST = 'smtp.mail.ru';
-		 const LOGIN = 'di.riz@mail.ru';
+		 const LOGIN = 'di.riz';
 		 const PASS = '98mvE1dM4Lw8AsHSsLnV';
 		 const PORT = '465';
 
 		// *** /SMTP *** //
    
     const SENDER = 'di.riz@mail.ru';
-    const CATCHER = 'mr.smokimo620@mail.ru';
+    const CATCHER = 'di.riz@mail.ru';
     const SUBJECT = 'Заявка с сайта';
     const CHARSET = 'UTF-8';
     
