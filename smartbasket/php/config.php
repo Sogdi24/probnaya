766cc209b54ff1fe6de40d72f18ba50a500f1383
Sogdiana Rizaeva <141143532+Sogdi24@users.noreply.github.com>
2024-06-04 23:09:03 +0300

    Update config.php

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -1,9 +1,9 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
+    require_once($_SERVER['DOCUMENT_ROOT'] . './smartbasket/php/phpmailer/phpmailer.php');
 
 		// *** SMTP *** //
 
-		 require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		 require_once($_SERVER['DOCUMENT_ROOT'] . './smartbasket/php/phpmailer/smtp.php');
 		 const HOST = 'smtp.mail.ru';
 		 const LOGIN = 'di.riz@mail.ru';
 		 const PASS = '98mvE1dM4Lw8AsHSsLnV';
